46bcaf054f010307541ad3a53498d41d28e0790d
Alex <alexzloid@gmail.com>
2023-08-07 01:30:59 +0500

    feat: add arrays text numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+	const h = [
+			'one hundred',
+			'two hundred',
+			'three hundred',
+			'four hundred',
+			'five hundred',
+			'six hundred',
+			'seven hundred',
+			'eight hundred',
+			'nine hundred',
+		],
+		t = [
+			'',
+			'twenty',
+			'thirty',
+			'fourty',
+			'fifty',
+			'sixty',
+			'seventy',
+			'eighty',
+			'ninety',
+		],
+		o = [
+			'one',
+			'two',
+			'three',
+			'four',
+			'five',
+			'six',
+			'seven',
+			'eight',
+			'nine',
+			'ten',
+		],
+		p = [
+			'eleven',
+			'twelve',
+			'thirteen',
+			'fourteen',
+			'fifteen',
+			'sixteen',
+			'seventeen',
+			'eighteen',
+			'nineteen',
+		];
+};
